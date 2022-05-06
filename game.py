2318ab3ed78576d48109769089eeffed77a0f1e0
Kysy32 <jkysely@centrum.cz>
2022-05-06 20:20:46 +0200

    Introduction

--- game.py
+++ game.py
@@ -4,3 +4,30 @@ author: Jakub Kyselý
 email: jkysely@centrum.cz
 discord: Kysy#6104
 """
+import random
+
+SEPARATOR = '-' * 47
+
+def introduction():
+    '''
+    Print greeting and game rules
+    '''
+    return print(f"Hi there\n"
+            f"{SEPARATOR}\n"
+            f"I've generated a random 4 digit number for you.\n"
+            f"Let's play a bulls and cows game.\n"
+            f"{SEPARATOR}")
+
+#introduction()
+
+def game():
+    game = True
+    secret = [random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9)]
+
+
+
+def play():
+    introduction()
+    return result
+
+game()
